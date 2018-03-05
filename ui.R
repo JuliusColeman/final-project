32ea190e9543a060a43b3efa3ca0476393c31daf
林麟 <llin124@D-108-179-146-248.dhcp4.washington.edu>
2018-03-04 20:21:00 -0800

    changes

--- ui.R
+++ ui.R
@@ -4,11 +4,11 @@ library("dplyr")
 # Load data
 police_report <- read.csv("Seattle_Police_Department_Police_Report_Incident.csv.bz2")
 
-crime_types <- c(animal, assault, burglary, counterfeit, dispute, disturbance, dui, 
-                 embezzle, false_report, forgery_check, fraud, harassment, homicide,
-                 injury, liquor_law, malicious_harassment, narcotics, obstruct, 
-                 property_damage, prostitution, reckless_burning, robbery, theft,
-                 viol_court, warrant, weapon)
+crime_types <- c("animal", "assault", "burglary", "counterfeit", "dispute", "disturbance", "dui", 
+                 "embezzle", "false report", "forgery check", "fraud", "harassment", "homicide",
+                 "injury", "liquor law", "malicious harassment", "narcotics", "obstruct", 
+                 "property damage", "prostitution", "reckless burning", "robbery", "theft",
+                 "violation of court order", "warrant", "weapon")
 
 # Define UI
 ui <- fluidPage(
