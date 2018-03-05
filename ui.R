d2053f705a4344f21296928b369601e54f7292a2
Julius Coleman <jc0315@uw.edu>
2018-03-04 19:35:44 -0800

    made some changes

--- ui.R
+++ ui.R
@@ -10,15 +10,16 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
 
-      sliderInput(inputId = "year", label = strong("Year"), min = 2012, max = 2018,
-                  value = 2016)
+      sliderInput(inputId = "year", label = strong("Year"), min = 2008, max = 2017,
+                  value = 2017)
     ),
     
     mainPanel(
       tabsetPanel(
-        tabPanel("tab 1", "contents"),
-        tabPanel("tab 2", "contents"),
-        tabPanel("tab 3", "contents"))
+        tabPanel("Home", "home"),
+        tabPanel("Crime Types", "crimetype"),
+        tabPanel("Time", "time"),
+        tabPanel("Map", "map"))
       
     )
   )
