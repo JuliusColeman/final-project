a6fbc02782c396bf3685ba6699ea70527022133c
Ryan Dang <dangryan@uw.edu>
2018-03-04 19:32:19 -0800

    added marker popups

--- locations_map.R
+++ locations_map.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(leaflet)
 library(readr)
+library(htmltools)
 
 data <- read_csv("~/Documents/Info 201 Assignments/final-project/Seattle_Police_Department_Police_Report_Incident.csv.zip") %>% 
   filter(Year == 2017)
@@ -9,7 +10,9 @@ map = leaflet(data) %>%
   addTiles() %>% 
   addMarkers(
     lng = ~Longitude, lat = ~Latitude,
+    popup = ~htmlEscape(Summarized.Offense.Description),
     clusterOptions = markerClusterOptions()
+    
   )
 
 print(map)
