7ccea5f3c13eb29e5cd5e76481b80cc4a8979588
林麟 <llin124@D-108-179-146-248.dhcp4.washington.edu>
2018-03-04 20:12:04 -0800

    made same changes to ui

--- ui.R
+++ ui.R
@@ -4,23 +4,32 @@ library("dplyr")
 # Load data
 police_report <- read.csv("Seattle_Police_Department_Police_Report_Incident.csv.bz2")
 
+crime_types <- c(animal, assault, burglary, counterfeit, dispute, disturbance, dui, 
+                 embezzle, false_report, forgery_check, fraud, harassment, homicide,
+                 injury, liquor_law, malicious_harassment, narcotics, obstruct, 
+                 property_damage, prostitution, reckless_burning, robbery, theft,
+                 viol_court, warrant, weapon)
+
 # Define UI
 ui <- fluidPage(
   titlePanel("Seattle Crime Rate"),
   sidebarLayout(
     sidebarPanel(
-
+      selectInput(inputId = "type", label = strong("Types of Crime"),
+                  choices = crime_types,
+                  selected = "animal"),
+      
       sliderInput(inputId = "year", label = strong("Year"), min = 2008, max = 2017,
                   value = 2017)
+      )
     ),
     
     mainPanel(
       tabsetPanel(
-        tabPanel("Home", "home"),
-        tabPanel("Crime Types", "crimetype"),
-        tabPanel("Time", "time"),
-        tabPanel("Map", "map"))
-      
+        tabPanel("Home", verbatimTextOutput("home")),
+        tabPanel("Crime Types", verbatimTextOutput("crimetypes")),
+        tabPanel("Time", plotOutput("time")),
+        tabPanel("Map", imageOutput("map"))
     )
   )
 )
