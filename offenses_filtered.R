8b895f74cb6a4adf6cc32472a7cb214f310e9191
Jordan Lucas <jordanlucas@D-108-179-151-140.dhcp4.washington.edu>
2018-03-07 11:53:38 -0800

    fixed crime type graph

--- offenses_filtered.R
+++ offenses_filtered.R
@@ -2,23 +2,24 @@
 library('dplyr')
 library('stringr')
 library('ggplot2')
-## reads data
-datat <- read.csv("~/Desktop/info_201/final-project/offenses/Seattle_Police_Department_Police_Report_Incident.csv.bz2", header = TRUE)
 
-## Filters offenses and removes spacing + "-"
+# reads data
+datat <- read.csv("~/Desktop/info_201/final-project/Seattle_Police_Department_Police_Report_Incident.csv.bz2", header = TRUE)
+
+# Filters offenses and removes spacing + "-"
 datatest <- select(datat, Offense.Type, Year)
 datatest$Offense.Type <- gsub('-.*',"",datatest$Offense.Type)
 datatest$Offense.Type <- gsub(' .*',"",datatest$Offense.Type)
 
-## focuses on the most common offenses 
+# focuses on the most common offenses 
 target <- c("WARRARR", "VEH", "THEFT", "ROBBERY", "NARC", "PROPERTY", "HARASSMENT", "FRAUD", "DISTURBANCE", "BURGLARY", "ASSLT")
 newdata <- datatest%>%
   filter(Offense.Type %in% target)
 
-## example of 2014 crime data 
-year.graph <- newdata[newdata$Year == "2014",]
+# example of 2014 crime data 
+year.graph <- newdata[newdata$Year == "2008",]
 
-## creates graph for the crimes 
+# creates graph for the crimes 
 offeneses_graph <- ggplot(data = year.graph) +
   geom_bar(width = .2, mapping = aes(x = Offense.Type, fill = Offense.Type)) + ggtitle("Offenses in Seattle") + coord_flip()+ scale_y_continuous(name="Freq", labels = scales::comma)
 
--- ui.R
+++ ui.R
@@ -35,14 +35,12 @@ shinyUI(navbarPage(title = "Seattle Crime Rate",
   tabPanel("Crime Types",
     # title of page
     h1("Graph of Crimes in Seattle", align ="center"),
-    h5("This interactive graph displays the most common crimes in Seattle. You can select a year from 2000 and up and it will display
+    h5("This interactive graph displays the most common crimes in Seattle. You can select a year from 2008 and up and it will display
        a graph showing different crimes that have been committed in the selected year. This will allow the user to see what the 
        most/least common crime was."),
       sidebarLayout(
         sidebarPanel(
-          selectInput("select", "Select Year:", choices = list( "2000" = "2000", 
-                      "2001" = "2001", "2002" = "2002", "2003"= "2003", "2004" = "2004", "2005" = "2005",
-                      "2006"="2006", "2007"="2007", "2008"="2008", "2009"="2009", "2010"="2010","2011" = "2011",
+          selectInput("select", "Select Year:", choices = list( "2008"="2008", "2009"="2009", "2010"="2010","2011" = "2011",
                       "2012" = "2012","2013"="2013","2014"="2014","2015"="2015","2016"="2016","2017"= "2017",
                       "2018" = "2018"))
         ),
